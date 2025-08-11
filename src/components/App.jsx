f8607a2e0fcd5efead0e3bef5fa75cc5e581a9eb
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-08-11 12:55:04 +0300

    в
    в

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from 'components/SharedLayout/SharedLayout';
+
+const Home = React.lazy(() => import('pages/Home/Home'));
+const Movies = React.lazy(() => import('pages/Movies/Movies'));
+const MovieDetails = React.lazy(() => import('pages/MovieDetails/MovieDetails'));
+const NotFound = React.lazy(() => import('pages/NotFound/NotFound'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <SharedLayout>
+        <div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movies" element={<Movies />} />
+              <Route path="/movies/:movieId/*" element={<MovieDetails />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </div>
+      </SharedLayout>
     </div>
   );
 };
+
+export default App;
